480b9434e36820994cb37c8d78540524697d5b0f
vit2 <vit2@ab.ru>
2021-04-05 23:45:50 +0200

    geometry

--- frontend/src/components/geom/geom.js
+++ frontend/src/components/geom/geom.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Button, Form, Row, Col } from "react-bootstrap";
 
+import triangle from "img/triangle.png";
+
 const Geom = () => {
   const [angA, setAngA] = useState(5);
   const [ans, setAns] = useState("");
+  const [result, setResult] = useState("");
+  const [summary, setSummary] = useState(0);
 
   const change = (e) => {
     setAns(e.target.value);
@@ -20,9 +24,18 @@ const Geom = () => {
   const click = () => {
     const a = random(4, 80);
     setAngA(a);
+    setResult("");
+    setAns("");
   };
   const clickAns = () => {
     console.log(ans);
+    const r = 180 - (angA + angA / 2);
+    if (r == ans) {
+      setResult("Правильно");
+      setSummary((prev) => prev + 1);
+    } else {
+      setResult(`Нет, правильно ${r}`);
+    }
   };
   useEffect(() => {
     const a = random(1, 80);
@@ -30,23 +43,47 @@ const Geom = () => {
   }, []);
   return (
     <div className="container">
+      <span>-------</span>
+      <br />
+      <img
+        src={triangle}
+        width="200"
+        height="200"
+        className="d-inline-block align-top"
+        alt="xx"
+      />
+      <br />
       <span>Дано - треугольник ABC, у которого AB = BC, </span>
       <br />
       <span>CK - биссектриса</span>
       <br />
-      <span>A = {angA} град.</span>
+      <span>угол A = {angA} град.</span>
       <br />
 
-      <Form.Control
-        type="text"
-        placeholder="ответ"
-        className="w-4"
-        value={ans}
-        onChange={change}
-      />
-      <Button variant="primary" onClick={clickAns} className="ml-2">
-        Ответ
-      </Button>
+      <Row className="mt-2">
+        <Col>
+          <Form.Control
+            type="text"
+            placeholder="ответ"
+            className="w-4"
+            value={ans}
+            onChange={change}
+          />
+        </Col>
+        <Col>
+          <Button variant="primary" onClick={clickAns} className="ml-2">
+            Ответ
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col md={3}>
+          <span>{result}</span>
+        </Col>
+        <Col>
+          <span>Правильных ответов: {summary}</span>
+        </Col>
+      </Row>
       <Button variant="success" onClick={click} className="mt-2">
         Еще
       </Button>
